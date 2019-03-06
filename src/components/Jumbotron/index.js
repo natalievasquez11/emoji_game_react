d1a208e55e0356fb5c5b864f04abc70dbc87aaa3
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-03-06 15:49:52 -0600

    navbar & header added

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return(
+    <header className="header">
+      <h1>Emoji Game</h1>
+      <h4>Click on the emojis to win points, but be sure not to click on the same emoji twice or its GAME OVER!</h4>
+    </header>
+  );
+}
+
+export default Jumbotron;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar-fixed">
+      <nav className="grey darken-3">
+        <div className="nav-wrapper">
+
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+
+export default Navbar;
