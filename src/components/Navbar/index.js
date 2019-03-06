d0c7c92a64f06cee524865d00ff71a5d3b5633e3
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-03-06 16:10:38 -0600

    styled navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-fixed">
+      <nav className="grey darken-3">
+      <div className="nav-wrapper">
+          <ul>
+            <li>Emoji Game</li>
+            <li>Start by clicking emojis!</li> 
+            <li>Points: 0 | Best Score: 0</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+
+export default Navbar;
