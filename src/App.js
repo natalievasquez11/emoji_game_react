0401ed33a0c185e428305809869cc86840ba8508
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-03-06 16:54:47 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron/index";
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Navbar />
         <Jumbotron />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return(
+        <footer className="page-footer grey darken-2">
+          <div className="footer-copyright grey darken-3">
+            Emoji Game
+          </div>
+        </footer>
+    );
+}
+
+export default Footer;
