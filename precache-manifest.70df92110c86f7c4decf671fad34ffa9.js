2ef7ad9ce6749ab810c14f082506eb5b520172cf
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-03-07 21:08:11 -0600

    Updates

---
+++ precache-manifest.70df92110c86f7c4decf671fad34ffa9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "65aed047a1c34db18e23edabd68ee3f7",
+    "url": "/emoji_game_react/static/media/Hushed.65aed047.png"
+  },
+  {
+    "revision": "d31d7059b6efc4c9f92f",
+    "url": "/emoji_game_react/static/css/main.9aa14c7e.chunk.css"
+  },
+  {
+    "revision": "d36b98b06f3837b96027",
+    "url": "/emoji_game_react/static/js/runtime~main.d36b98b0.js"
+  },
+  {
+    "revision": "11c0a7a100187e21d1e7",
+    "url": "/emoji_game_react/static/js/2.11c0a7a1.chunk.js"
+  },
+  {
+    "revision": "ccb112edb63776487554e0d11e2bf8ec",
+    "url": "/emoji_game_react/static/media/Angry.ccb112ed.png"
+  },
+  {
+    "revision": "d7c13e7161fd40d1d34bea348ef15348",
+    "url": "/emoji_game_react/static/media/Confounded.d7c13e71.png"
+  },
+  {
+    "revision": "6a36157ff788974dacf598d186b084c2",
+    "url": "/emoji_game_react/static/media/Flushed.6a36157f.png"
+  },
+  {
+    "revision": "b19e3fe84a7fa3085561f65af28859f4",
+    "url": "/emoji_game_react/static/media/Halo.b19e3fe8.png"
+  },
+  {
+    "revision": "d31d7059b6efc4c9f92f",
+    "url": "/emoji_game_react/static/js/main.d31d7059.chunk.js"
+  },
+  {
+    "revision": "73b79cc18e1846f86514aa32a320bb42",
+    "url": "/emoji_game_react/static/media/Loudly_Crying.73b79cc1.png"
+  },
+  {
+    "revision": "f1eede5aa877fb9398ca25fb89c8ac7d",
+    "url": "/emoji_game_react/static/media/Mad.f1eede5a.png"
+  },
+  {
+    "revision": "39c831077c935e2f0e663601935353de",
+    "url": "/emoji_game_react/static/media/Sunglasses.39c83107.png"
+  },
+  {
+    "revision": "d5d15845366c69909f760a128f1e7cc3",
+    "url": "/emoji_game_react/static/media/Tears.d5d15845.png"
+  },
+  {
+    "revision": "58beb49d6eca6ea38e1621177ccac356",
+    "url": "/emoji_game_react/static/media/Tightly_Closed.58beb49d.png"
+  },
+  {
+    "revision": "53a62f3a198241a4c3a9fa721bb9114f",
+    "url": "/emoji_game_react/static/media/Tongue_Winking.53a62f3a.png"
+  },
+  {
+    "revision": "a4507f22f80321cd842af54a69d661e5",
+    "url": "/emoji_game_react/static/media/Upside-Down.a4507f22.png"
+  },
+  {
+    "revision": "749eb9b5432ddc98e99985103014b64b",
+    "url": "/emoji_game_react/index.html"
+  }
+];
